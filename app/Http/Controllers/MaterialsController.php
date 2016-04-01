b96fa3737de9d640c77f2b50cbdc1899cea622fd
Karthik SWOT <nkmswot@users.noreply.github.com>
2016-04-01 17:15:08 +0530

    Update MaterialsController.php

--- app/Http/Controllers/MaterialsController.php
+++ app/Http/Controllers/MaterialsController.php
@@ -164,8 +164,8 @@ class MaterialsController extends Controller
                 //   'm_company'                 =>$row->m_company,
                  //  'm_location'                =>$row->m_location,
                  //  'm_reorder'                 =>$row->m_reorder,
-                   'm_last_update_date'        =>date("d-M-Y g:i A",strtotime($row->m_last_update_date)),
-                   'm_mesc'                    =>$row->m_mesc,
+                 //  'm_last_update_date'        =>date("d-M-Y g:i A",strtotime($row->m_last_update_date)),
+                  // 'm_mesc'                    =>$row->m_mesc,
                    'user_id'                   =>Auth::user()->id
 
 
